18df0e3f7391f2ccc1f198d8dfe6edcf8181f849
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-01-05 16:44:59 -0400

    Initial infrastructure for get_repos lambda

---
+++ lambdas.tf
@@ -0,0 +1,53 @@
+#####################
+# General Config
+
+data "aws_iam_policy_document" "assume_role_lambda_policy" {
+  statement {
+    sid = "lambdaAssumeRolePolicy"
+
+    actions = ["sts:AssumeRole"]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "assume_role_lambda" {
+  name = "AssumeRoleLambda"
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_lambda_policy.json
+}
+
+#####################
+# Get Repos Lambda
+
+locals {
+  get_repos_name = "get_repos"
+}
+
+data "archive_file" "get_repos_archive_file" {
+  type = "zip"
+
+  source_dir  = "${path.module}/${local.get_repos_name}"
+  output_path = "${path.module}/${local.get_repos_name}.zip"
+}
+
+resource "aws_lambda_function" "get_repos" {
+  filename      = "${local.get_repos_name}.zip"
+  function_name = local.get_repos_name
+  handler       = "${local.get_repos_name}.handler"
+  role          = aws_iam_role.assume_role_lambda.arn
+
+  runtime          = "Python 3.9"
+  source_code_hash = data.archive_file.get_repos_archive_file.output_base64sha256
+}
+
+resource "aws_cloudwatch_log_group" "get_repos_log_group" {
+  name = "aws/lambda/${aws_lambda_function.get_repos.function_name}"
+
+  retention_in_days = 30
+}
---
+++ lambdas/get_repos.py
@@ -0,0 +1,30 @@
+import json
+import urllib.request as request
+
+def get_repos():
+    """
+    Gets repos for the configured user
+
+    Returns
+    -------
+    json
+        a json object representing a list of repositories
+    """
+
+    response = request.urlopen(
+            request.Request(
+                url='https://api.github.com/users/jacobtleblanc/repos',
+                headers={'Accept': 'application/json'},
+                method='GET'
+            ),
+            timeout=5
+        )
+
+    return json.loads(response.read())
+
+def lambda_handler(event, context):
+
+    return {
+        'statusCode': 200,
+        'body': get_repos()
+    }
