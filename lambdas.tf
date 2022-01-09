16a7f9aefa4b0c3704afddca8deeadf77b0ac124
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-01-08 23:32:44 -0400

    Added trigger to api gateway deployment

--- lambdas.tf
+++ lambdas.tf
@@ -64,5 +64,5 @@ resource "aws_lambda_permission" "apigw_get_repos" {
   function_name = aws_lambda_function.get_repos.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.git.id}/*/${aws_api_gateway_method.get_repos_method.http_method}${aws_api_gateway_resource.get_repos_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.git.id}/*/${aws_api_gateway_method.get_repos_method.http_method}${aws_api_gateway_resource.get_repos_user_resource.path}"
 }
