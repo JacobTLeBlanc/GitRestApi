757215608c70a81184beb07ae3db024845e94158
Jacob LeBlanc <leblancthomasjacob@gmail.com>
2022-01-09 21:27:58 -0400

    Added headers to lmbda response

--- lambdas/get_repos.py
+++ lambdas/get_repos.py
@@ -33,5 +33,9 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+        },
         'body': json.dumps(get_repos(user))
     }
